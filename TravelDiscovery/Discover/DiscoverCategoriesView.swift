5e3bb6f87d11c8d0567ede56598197a9917f68a5
Vinh Le <lethanhvinh95@gmail.com>
2021-01-27 07:22:58 +0200

    render category details view in NaviationLink

--- TravelDiscovery/Discover/DiscoverCategoriesView.swift
+++ TravelDiscovery/Discover/DiscoverCategoriesView.swift
@@ -22,27 +22,53 @@ struct DiscoverCategoriesView: View {
             // A horizontal stack of rounded icons
             HStack(spacing: 12) {
                 ForEach(categories, id: \.self) { category in
-                    VStack(content: {
-                        Image(systemName: category.iconName)
-                            .frame(width: 68, height: 68, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            .foregroundColor(Color.primaryColor)
-                            .background(Color.white)
-                            .cornerRadius(34)
-                            .shadow(color: .gray, radius: 5, x: 0.0, y: 2)
-                        
-                        Text(category.title)
-                            .smallSemiboldText()
-                            .padding(EdgeInsets(top: 4, leading: 0, bottom: 0, trailing: 0))
-                            .foregroundColor(.white)
-                    })
+                    NavigationLink(
+                        destination: CategoryDetailsView(),
+                        label: {
+                            VStack(content: {
+                                Image(systemName: category.iconName)
+                                    .frame(width: 68, height: 68, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    .foregroundColor(Color.primaryColor)
+                                    .background(Color.white)
+                                    .cornerRadius(34)
+                                    .shadow(color: .gray, radius: 5, x: 0.0, y: 2)
+                                
+                                Text(category.title)
+                                    .smallSemiboldText()
+                                    .padding(EdgeInsets(top: 4, leading: 0, bottom: 0, trailing: 0))
+                                    .foregroundColor(.white)
+                            })
+                        }
+                    )
                 }
             }.padding(.horizontal)
         })
     }
 }
 
+struct CategoryDetailsView: View {
+    var body: some View {
+        ScrollView {
+            ForEach(1..<3, id: \.self) { num in
+                VStack(alignment: .leading, spacing: 0) {
+                    Image("art\(num)")
+                        .resizable()
+                        .scaledToFill()
+                        .tileStyle()
+                    Text("Image title")
+                        .smallSemiboldText()
+                }.padding()
+            }
+        }.navigationBarTitle("Category", displayMode: .inline)
+    }
+}
+
 struct DiscoverCategoriesView_Previews: PreviewProvider {
     static var previews: some View {
-        DiscoverCategoriesView()
+//        DiscoverCategoriesView()
+        NavigationView {
+            CategoryDetailsView()
+        }
+        DiscoverView()
     }
 }
